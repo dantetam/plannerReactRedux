8f221fbfa73f2997c671c71561bd94e96e859392
dtam <dtam@paypal.com>
2018-06-14 11:33:02 -0700

    Webpack proxy POST request

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,26 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new HtmlWebPackPlugin({
+        template: "./src/testpoint.html",
+        filename: "./testpoint.html"
     })
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/getproxy': {
+        bypass: (req, res) => {
+          res.send("This is a proxy GET request.");
+        }
+      },
+      '/postproxy': {
+        bypass: (req, res) => {
+            if(req.method == 'POST') {
+                res.send("This is a proxy POST request.")
+            }
+        }  
+      }
+    }
+  }
 };
